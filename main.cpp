394e5b7ef5ee0985e2d7e6c04302ef54324afa1d
lucmobz <luca.mobz.work@gmail.com>
2022-10-23 18:00:45 +0200

    fix bug with non moving packet

--- main.cpp
+++ main.cpp
@@ -15,77 +15,82 @@ static constexpr auto get_potential(double x, double y) { return 0.0; }
 
 void set_matrix(SparseMatrix<std::complex<double>>& A, const VectorXd& xs,
                 const VectorXd& ys, double dt) {
+  using namespace std::complex_literals;
+
   auto dx = xs[1] - xs[0];
   auto dy = ys[1] - ys[0];
-  std::complex<double> ct{0.0, dt};
-  auto cx = 0.5 * ct / (dx * dx);
-  auto cy = 0.5 * ct / (dy * dy);
-  auto cxy = 1.0 + ct * (1.0 / (dx * dx) + 1.0 / (dy * dy));
+  auto ct = 1.0i * dt;
+  auto cx = ct / (4.0 * dx * dx);
+  auto cy = ct / (4.0 * dy * dy);
 
-  std::vector<Triplet<std::complex<double>>> nnz;
+  std::vector<Triplet<std::complex<double>>> nz;
   const auto get_id = [n = xs.size()](auto j, auto i) { return j + i * n; };
 
   for (auto i = 0; i < ys.size(); ++i) {
     for (auto j = 0; j < xs.size(); ++j) {
       auto v = get_potential(xs[j], ys[i]);
+      auto cxy = 1.0 + ct / 2.0 * (v + 1.0 / (dx * dx) + 1.0 / (dy * dy));
       auto id = get_id(j, i);
 
-      nnz.emplace_back(id, id, cxy - 0.5 * ct * v);
-      if (j - 1 >= 0) nnz.emplace_back(id, get_id(j - 1, i), -cx);
-      if (j + 1 < xs.size()) nnz.emplace_back(id, get_id(j + 1, i), -cx);
-      if (i - 1 >= 0) nnz.emplace_back(id, get_id(j, i - 1), -cy);
-      if (i + 1 < ys.size()) nnz.emplace_back(id, get_id(j, i + 1), -cy);
+      nz.emplace_back(id, id, cxy);
+      if (j - 1 >= 0) nz.emplace_back(id, get_id(j - 1, i), -cx);
+      if (j + 1 < xs.size()) nz.emplace_back(id, get_id(j + 1, i), -cx);
+      if (i - 1 >= 0) nz.emplace_back(id, get_id(j, i - 1), -cy);
+      if (i + 1 < ys.size()) nz.emplace_back(id, get_id(j, i + 1), -cy);
     }
   }
 
-  A.setFromTriplets(nnz.begin(), nnz.end());
+  A.setFromTriplets(nz.begin(), nz.end());
 }
 
 void set_rhs(VectorXcd& b, const VectorXcd& u0, const VectorXd& xs,
              const VectorXd& ys, double dt) {
+  using namespace std::complex_literals;
+
   auto dx = xs[1] - xs[0];
   auto dy = ys[1] - ys[0];
-  std::complex<double> ct{0.0, dt};
-  auto cx = 0.5 * ct / (dx * dx);
-  auto cy = 0.5 * ct / (dy * dy);
-  auto cxy = 1.0 - ct * (1.0 / (dx * dx) + 1.0 / (dy * dy));
+  auto ct = 1.0i * dt;
+  auto cx = ct / (4.0 * dx * dx);
+  auto cy = ct / (4.0 * dy * dy);
 
   const auto get_id = [n = xs.size()](auto j, auto i) { return j + i * n; };
 
   for (auto i = 0; i < ys.size(); ++i) {
     for (auto j = 0; j < xs.size(); ++j) {
       auto v = get_potential(xs[j], ys[i]);
+      auto cxy = 1.0 - ct / 2.0 * (v + 1.0 / (dx * dx) + 1.0 / (dy * dy));
       auto id = get_id(j, i);
 
-      b[id] = u0[id] * (cxy + 0.5 * ct * v);
+      b[id] = u0[id] * cxy;
       if (j - 1 >= 0) b[id] += u0[get_id(j - 1, i)] * cx;
       if (j + 1 < xs.size()) b[id] += u0[get_id(j + 1, i)] * cx;
       if (i - 1 >= 0) b[id] += u0[get_id(j, i - 1)] * cy;
-      if (i + 1 < ys.size()) b[id] = u0[get_id(j, i + 1)] * cy;
+      if (i + 1 < ys.size()) b[id] += u0[get_id(j, i + 1)] * cy;
     }
   }
 }
 
 struct GaussianWavePacket {
-  double x0 = 0.0;
-  double y0 = 0.0;
+  double x0 = 0.5;
+  double y0 = 0.5;
   double sigma = 1.0;
-  double kx = 0.0;
-  double ky = 0.0;
+  double kx = 1.0;
+  double ky = 1.0;
+  double sigma2 = sigma * sigma;
 
   auto operator()(double x, double y) const {
     auto dx = x - x0;
     auto dy = y - y0;
-    auto r = std::exp(-0.5 * (dx * dx + dy * dy) / sigma / sigma) /
-             (std::numbers::pi * sigma * sigma);
-    auto theta = -(kx * dx + ky * dy);
+    auto r = std::exp(-0.5 * (dx * dx + dy * dy) / sigma2) /
+             (std::numbers::pi * sigma2);
+    auto theta = kx * dx + ky * dy;
     return std::polar(r, theta);
   }
 };
 
 void set_initial_condition(VectorXcd& u0, const VectorXd& xs,
                            const VectorXd& ys) {
-  GaussianWavePacket gwp{0.2, 0.5, 0.05, 1.0, 0.0};
+  GaussianWavePacket gwp{0.2, 0.4, 0.05, 10000.0/*std::numbers::pi * 15.0*/, 0.0};
 
   for (auto i = 0; i < ys.size(); ++i)
     for (auto j = 0; j < xs.size(); ++j)
@@ -98,22 +103,22 @@ void output_solution(std::ostream& os, const VectorXcd& u, const VectorXd& xs,
     for (auto j = 0; j < xs.size(); ++j) {
       auto id = j + i * xs.size();
       os << std::setprecision(15) << xs[j] << ' ' << ys[i] << ' '
-         << std::norm(u[id]) << ' ' << u[id].real() << ' ' << u[id].imag()
-         << '\n';
+         << std::norm(u[id]) << ' '
+         << u[id].real() << ' ' << u[id].imag() << '\n';
     }
     os << '\n';
   }
 }
 
 int main() {
-  auto nx = 512;
+  auto nx = 64;
   auto ny = nx;
   auto sz = nx * ny;
   VectorXd xs = VectorXd::LinSpaced(nx, 0.0, 1.0);
   VectorXd ys = xs;
   auto dx = xs[1] - xs[0];
   auto dy = dx;
-  auto dt = dx * dy * 1.0e-1;
+  auto dt = dx * dy / 4;
 
   SparseMatrix<std::complex<double>> A(sz, sz);
   VectorXcd b;
@@ -126,7 +131,8 @@ int main() {
   set_initial_condition(u0, xs, ys);
   set_matrix(A, xs, ys, dt);
   set_rhs(b, u0, xs, ys, dt);
-  SimplicialLDLT<decltype(A)> chol(A);
+  SparseLU<decltype(A)> chol;
+  chol.compute(A);
 
   if (chol.info() != Success) {
     std::cerr << "Error\n";
@@ -137,13 +143,14 @@ int main() {
   std::cout << "Solving dofs: " << sz << "\n";
 
   auto i = 0;
-  for (auto t = 0.0; i < 32; ++i, t += dt) {
+  for (auto t = 0.0; i < 512; ++i, t += dt) {
     ofs.open("./data/u" + std::to_string(i) + ".dat");
     output_solution(ofs, u0, xs, ys);
     ofs.close();
 
     u = chol.solve(b);
     u0 = u;
+    b.setZero();
     set_rhs(b, u0, xs, ys, dt);
 
     std::cout << "Solved iteration: " << i << ", time: " << t << "\n";
--- plot.gnu
+++ plot.gnu
@@ -1,6 +1,7 @@
 set size square
 set pm3d map
-do for [i=0:32] {
+do for [i=0:512] {
+  set title 'i: '.i.' iter'
   splot './data/u'.i.'.dat' u 1:2:3 with pm3d
-  pause 0.25
+  pause 0.1
 }
